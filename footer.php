840c3b34b0a5f8e61b5102c71145090f03a0c066
MuhammadFillah <muhammadfillah30@gmail.com>
2026-05-06 02:35:10 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<Footer>
+    <div class="legal">
+        <div class="copyright">
+            &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
+        </div>
+        <div class="version">
+            <b>Version: </b> 1.0.5
+        </div>
+    </div>
+</Footer>
+
+<!-- Jquery Core Js -->
+<script src="plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="plugins/bootstrap/js/bootstrap.js"></script>
+
+<!-- Waves Effect Plugin Js -->
+<script src="plugins/node-waves/waves.js"></script>
+
+<!-- Slimscroll Plugin Js -->
+<script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+<!-- Custom Js -->
+<script src="js/admin.js"></script>
+
+<!-- Demo Js -->
+<script src="js/demo.js"></script>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="id">
+<?php include 'inc/header.php'; ?>
+<link href="css/style.css" rel="stylesheet">
+
+<body>
+
+   <?php include 'inc/sidebar.php'; ?>
+   <?php include 'inc/footer.php'; ?>
+
+</body>
+
+</html>
