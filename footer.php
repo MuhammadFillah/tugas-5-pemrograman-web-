0a47f138070f47b8a27c2aa037b4277d2a2a6856
MuhammadFillah <muhammadfillah30@gmail.com>
2026-05-06 20:01:28 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,30 +1,24 @@
-<?php
-?>
-<Footer>
-    <div class="legal">
-        <div class="copyright">
-            &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
-        </div>
-        <div class="version">
-            <b>Version: </b> 1.0.5
-        </div>
-    </div>
-</Footer>
-
 <!-- Jquery Core Js -->
 <script src="plugins/jquery/jquery.min.js"></script>
 
 <!-- Bootstrap Core Js -->
 <script src="plugins/bootstrap/js/bootstrap.js"></script>
 
-<!-- Waves Effect Plugin Js -->
-<script src="plugins/node-waves/waves.js"></script>
+<!-- Select Plugin Js -->
+<script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
 
 <!-- Slimscroll Plugin Js -->
 <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
 
+<!-- Waves Effect Plugin Js -->
+<script src="plugins/node-waves/waves.js"></script>
+
 <!-- Custom Js -->
 <script src="js/admin.js"></script>
+<script src="js/pages/index.js"></script>
 
 <!-- Demo Js -->
 <script src="js/demo.js"></script>
+</body>
+
+</html>
