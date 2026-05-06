c735c7af81a75beb11599e93d6bef0b94cc46da2
MuhammadFillah <muhammadfillah30@gmail.com>
2026-05-06 20:02:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <?php
+session_start();
+if (!isset($_SESSION['login'])) {
+   header("Location: login.php");
+   exit;
+}
+// Memanggil header
+include 'inc/header.php';
 
+// Memanggil sidebar
+include 'inc/sidebar.php';
 ?>
-<!DOCTYPE html>
-<html lang="id">
-<?php include 'inc/header.php'; ?>
-<link href="css/style.css" rel="stylesheet">
 
-<body>
+<section class="content">
+   <div class="container-fluid">
+      <div class="block-header">
+         <h2>DASHBOARD</h2>
+      </div>
 
-   <?php include 'inc/sidebar.php'; ?>
-   <?php include 'inc/footer.php'; ?>
+      <!-- Isi konten utama seperti Widgets, Charts, dan Tabel diletakkan di sini -->
+      <div class="row clearfix">
+         <!-- Contoh Widget -->
+         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-pink hover-expand-effect">
+               <div class="icon"><i class="material-icons">playlist_add_check</i></div>
+               <div class="content">
+                  <div class="text">NEW TASKS</div>
+                  <div class="number">125</div>
+               </div>
+            </div>
+         </div>
+      </div>
 
-</body>
+   </div>
+</section>
 
-</html>
+<?php
+// Memanggil footer
+include 'inc/footer.php';
+?>
